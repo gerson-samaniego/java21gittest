509212b1ff1738077ef9ef84c23228b1accc1a3a
Gerson Samaniego <gersonjohan@gmail.com>
2024-11-06 23:30:19 -0500

    GS in HW

--- src/main/java/com/mynic/java21gittest/App.java
+++ src/main/java/com/mynic/java21gittest/App.java
@@ -6,6 +6,6 @@ package com.mynic.java21gittest;
  */
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello World! GS");
 	}
 }
